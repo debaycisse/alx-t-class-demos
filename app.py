0e8832191602a1c7679bfdefbdbda951b84f0b52
azeez <debaycisse@gmail.com>
2022-07-26 04:40:18 +0100

    added some code to fetch from the DB

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 # Let's instantiate our app by creating an instance of the Flask class
 app = Flask(__name__) #This is a standard way of creating a flask app.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/example'
+
+
+db = SQLAlchemy(app)
+
+# Table creation in python env. via the help of SQLAlchemy
+class Person(db.Model):
+    __tablename__='persons'
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(), nullable=False)
+    def __repr__(self):
+        return f'<Person ID: {self.id}, Name: {self.name}>'
+
+db.create_all()
 
 # Then we declared a python decorator, from where we can tell the app of 'which endpoint' to listen to. And we will want it to listen to the homepage route. 
 @app.route('/')
-
 # So, whenever a user connect to the homepage, we want the user's connection to be handled by a method, which we are going to create right here below.
 def index():
-    return "Hello World!, this is DebayCisse."
+    person = Person.query.first()
+    return "Hello World! {}".format(person.name)
 
 
 # Below should always be included at the bottom of other codes
