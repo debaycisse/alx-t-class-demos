dec083856c1572d8694b555a05f06baa5511a149
azeez <debaycisse@gmail.com>
2022-07-26 05:00:42 +0100

    added code - how to insert and commit into the table

--- app.py
+++ app.py
@@ -18,6 +18,21 @@ class Person(db.Model):
 
 db.create_all()
 
+# To insert into the table
+# person1 = Person(name='Bosun')
+# person2 = Person(name='Seun')
+# person3 = Person(name='Ola')
+# person4 = Person(name='Ridwan')
+# person5 = Person(name='Tobi')
+# person6 = Person(name='Sarah')
+
+# # To add one person
+# db.session.add(person1)
+# # To add multiple persons
+# db.session.add_all([person2,person3,person4,person5,person6])
+# # To save this insertion or addition to the table
+# db.session.commit()
+
 # Then we declared a python decorator, from where we can tell the app of 'which endpoint' to listen to. And we will want it to listen to the homepage route. 
 @app.route('/')
 # So, whenever a user connect to the homepage, we want the user's connection to be handled by a method, which we are going to create right here below.
