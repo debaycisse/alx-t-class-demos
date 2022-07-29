82d418471f5741f22abf96bef0e658a4bfe21fac
azeez <debaycisse@gmail.com>
2022-07-29 06:19:59 +0100

    modified the code to fetch data from the DB

--- todoapplication/app.py
+++ todoapplication/app.py
@@ -6,17 +6,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:543
 
 db= SQLAlchemy(app)
 
+# Our model
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    # our debug method
+    def __repr__(self) -> str:
+        return f'<TODO ID:- {self.id}, DESCRIPTION:- {self.description}'
+
+# Let's create our modelled table
+db.create_all()
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description': 'Todo 1'
-    },{
-        'description': 'Todo 2'
-    },{
-        'description': 'Todo 3'
-    }
-])
+    return render_template('index.html', data=Todo.query.all())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
