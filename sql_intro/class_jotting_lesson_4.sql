38770dfa82e631b1b4b86bb0528c0c4ab7d4ef3c
debaycisse <debaycisse@gmail.com>
2022-12-30 15:43:45 +0100

    continue the study

--- sql_intro/class_jotting_lesson_4.sql
+++ sql_intro/class_jotting_lesson_4.sql
@@ -186,43 +186,74 @@
 *********************************************************/
     -- Examples
         -- Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.
-        SELECT a.name, MAX(o.occurred_at) AS order_date
-        FROM orders o
-        JOIN accounts a
-        ON o.account_id = a.id
-        GROUP BY a.name
-        LIMIT 1;
 
-        SELECT a.name
+        SELECT a.name, o.occurred_at
         FROM orders o
         JOIN accounts a
         ON o.account_id = a.id
-        ORDER BY o.occurred_at DESC
+        ORDER BY o.occurred_at
         LIMIT 1;
 
 
 
         -- Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders in usd and the company name.
-
+        SELECT a.name, SUM(o.total_amt_usd) AS total_sales
+        FROM orders o
+        JOIN accounts a
+        ON o.account_id = a.id
+        GROUP BY a.name;
 
 
         -- Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name.
+        SELECT MAX(w.occurred_at) AS order_date, w.channel, a.name
+        FROM web_events w
+        JOIN accounts a
+        ON w.account_id = a.id
+        GROUP BY a.name, w.channel
+        ORDER BY order_date DESC;
 
+        -- OR
+        SELECT w.occurred_at, w.channel, a.name
+        FROM web_events w
+        JOIN accounts a
+        ON w.account_id = a.id 
+        ORDER BY w.occurred_at DESC
+        LIMIT 1;
 
 
         -- Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used.
+        SELECT w.channel AS channels_name, COUNT(w.channel) AS number_of_channels
+        FROM web_events w
+        GROUP BY channels_name;
 
 
 
         -- Who was the primary contact associated with the earliest web_event?
-
+        SELECT a.primary_poc
+        FROM web_events w
+        JOIN accounts a
+        ON a.id = w.account_id
+        ORDER BY w.occurred_at
+        LIMIT 1;
 
 
         -- What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.
+        SELECT a.name, MIN(total_amt_usd) smallest_order
+        FROM accounts a
+        JOIN orders o
+        ON a.id = o.account_id
+        GROUP BY a.name
+        ORDER BY smallest_order;
 
 
 
         -- Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from the fewest reps to most reps.
+        SELECT r.name, COUNT(s.id) AS reps_counts
+        FROM sales_reps s
+        JOIN region r
+        ON s.region_id = r.id
+        GROUP BY r.name
+        ORDER BY reps_counts
 
 
 
@@ -233,11 +264,62 @@
 
 
 
-
-### 
+### GROUP BY Part II
 /*********************************************************
-    
+    Key takeaways:
+
+        You can GROUP BY multiple columns at once, as we showed here. This is often useful to aggregate across a number of different segments.
+
+        The order of columns listed in the ORDER BY clause does make a difference. You are ordering the columns from left to right.
+
+    GROUP BY - Expert Tips
+
+        The order of column names in your GROUP BY clause doesn’t matter—the results will be the same regardless. If we run the same query and reverse the order in the GROUP BY clause, you can see we get the same results.
+
+        As with ORDER BY, you can substitute numbers for column names in the GROUP BY clause. It’s generally recommended to do this only when you’re grouping many columns, or if something else is causing the text in the GROUP BY clause to be excessively long.
+
+        A reminder here that any column that is not within an aggregation must show up in your GROUP BY statement. If you forget, you will likely get an error. However, in the off chance that your query does work, you might not like the results!
 *********************************************************/
+-- Examples
+
+    -- For each account, determine the average amount of each type of paper they purchased across their orders. Your result should have four columns - one for the account name and one for the average quantity purchased for each of the paper types for each account.
+    SELECT a.name, AVG(o.standard_qty) AS standard_qty_avg, AVG(o.poster_qty) AS poster_qty_avg, AVG(o.gloss_qty) AS gloss_qty_avg
+    FROM orders o
+    JOIN accounts a
+    ON o.account_id = a.id
+    GROUP BY a.name;
+
+
+    -- For each account, determine the average amount spent per order on each paper type. Your result should have four columns - one for the account name and one for the average amount spent on each paper type.
+    SELECT a.name, AVG(o.standard_amt_usd) AS standard_amt_avg, AVG(o.poster_amt_usd) AS poster_amt_avg, AVG(o.gloss_amt_usd) AS gloss_amt_avg
+    FROM orders o
+    JOIN accounts a
+    ON o.account_id = a.id
+    GROUP BY a.name; 
+
+
+    -- Determine the number of times a particular channel was used in the web_events table for each sales rep. Your final table should have three columns - the name of the sales rep, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
+    SELECT s.name sales_rep_name, w.channel, COUNT(w.channel) web_channel_count
+    FROM web_events w
+    JOIN accounts a
+    ON w.account_id = a.id
+    JOIN sales_reps s
+    ON a.sales_rep_id = s.id
+    GROUP BY sales_rep_name, w.channel
+    ORDER BY web_channel_count DESC
+
+
+    -- Determine the number of times a particular channel was used in the web_events table for each region. Your final table should have three columns - the region name, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
+    SELECT r.name region_name, w.channel, COUNT(w.channel) number_of_occurences
+    FROM web_events w
+    JOIN accounts a
+    ON w.account_id = a.id
+    JOIN sales_reps s
+    ON a.sales_rep_id = s.id
+    JOIN region r
+    ON s.region_id = r.id
+    GROUP BY region_name, w.channel
+    ORDER BY number_of_occurences DESC
 
 
 
@@ -247,11 +329,61 @@
 
 
 
-
-### 
+### DISTINCT
 /*********************************************************
-    
+    DISTINCT is always used in SELECT statements, and it provides the unique rows for all columns written in the SELECT statement. Therefore, you only use DISTINCT once in any particular SELECT statement.
+
+    You could write:
+
+    SELECT DISTINCT column1, column2, column3
+    FROM table1;
+
+    which would return the unique (or DISTINCT) rows across all three columns.
+
+    You would not write:
+
+    SELECT DISTINCT column1, DISTINCT column2, DISTINCT column3
+    FROM table1;
+
+    You can think of DISTINCT the same way you might think of the statement "unique".
+
+    DISTINCT - Expert Tip
+    It’s worth noting that using DISTINCT, particularly in aggregations, can slow your queries down quite a bit.
 *********************************************************/
+-- Examples
+
+    -- Use DISTINCT to test if there are any accounts associated with more than one region.
+        -- The below two queries have the same number of resulting rows (351), so we know that every account is associated with only one region. If each account was associated with more than one region, the first query should have returned more rows than the second query.
+
+            SELECT a.id as "account id", r.id as "region id", 
+            a.name as "account name", r.name as "region name"
+            FROM accounts a
+            JOIN sales_reps s
+            ON s.id = a.sales_rep_id
+            JOIN region r
+            ON r.id = s.region_id;
+
+            -- and
+
+            SELECT DISTINCT id, name
+            FROM accounts;
+
+
+
+    -- Have any sales reps worked on more than one account?
+        -- Actually, all of the sales reps have worked on more than one account. The fewest number of accounts any sales rep works on is 3. There are 50 sales reps, and they all have more than one account. Using DISTINCT in the second query assures that all of the sales reps are accounted for in the first query.
+
+        SELECT s.id, s.name, COUNT(*) num_accounts
+        FROM accounts a
+        JOIN sales_reps s
+        ON s.id = a.sales_rep_id
+        GROUP BY s.id, s.name
+        ORDER BY num_accounts;
+
+        -- and
+
+        SELECT DISTINCT id, name
+        FROM sales_reps;
 
 
 
@@ -261,10 +393,143 @@
 
 
 
-### 
+
+
+### HAVING
 /*********************************************************
-    
+    HAVING - Expert Tip
+
+    HAVING is the “clean” way to filter a query that has been aggregated, but this is also commonly done using a subquery. Essentially, any time you want to perform a WHERE on an element of your query that was created by an aggregate, you need to use HAVING instead.
+
+    It is like WHERE clause but it works on logical statements involving aggregations.
+
+    It used when you need to filter aggregated columns result. And where the aggregated columns are just two or more but not for an entire table data.
+
+    It is placed after WHERE & GROUP BY but before the ORDER BY clauses.
 *********************************************************/
+    -- Examples
+    -- To filter accounts whose total purchase was at least 250,000USD
+    SELECT account_id, SUM(total_amt_usd) AS sum_total_amt_usd
+    FROM orders
+    GROUP BY 1
+    HAVING SUM(total_amt_usd) >= 250000
+
+
+
+    -- How many of the sales reps have more than 5 accounts that they manage?
+    SELECT s.name AS sales_rep_name, COUNT(a.sales_rep_id) account_occurences
+    FROM sales_reps s
+    JOIN accounts a
+    ON s.id = a.sales_rep_id
+    GROUP BY s.name
+    HAVING COUNT(a.sales_rep_id) > 5; 
+        -- There are 34 sales reps who manage more than 5 accounts.
+
+
+    -- How many accounts have more than 20 orders?
+    SELECT a.name, COUNT(o.*) number_of_orders
+    FROM accounts a
+    JOIN orders o
+    ON a.id = o.account_id
+    GROUP by a.name
+    HAVING COUNT(o.*) > 20;
+        -- There are 120 accounts with more than 20 orders
+
+
+    -- Which account has the most orders?
+    SELECT a.name, COUNT(o.*) number_of_orders
+    FROM accounts a
+    JOIN orders o
+    ON a.id = o.account_id
+    GROUP by a.name
+    ORDER BY 2 DESC
+    LIMIT 1;
+    -- OR
+    SELECT a.name, COUNT(o.*) number_of_orders
+    FROM accounts a
+    JOIN orders o
+    ON a.id = o.account_id
+    GROUP by a.name
+    HAVING COUNT(o.*) > 20
+    ORDER BY 2 DESC
+    LIMIT 1;
+        -- Leucadia National has the most orders
+
+
+    -- Which accounts spent more than 30,000 usd total across all orders?
+    SELECT a.name, SUM(o.standard_amt_usd + o.gloss_amt_usd + o.poster_amt_usd) AS orders_total_usd
+    FROM accounts a
+    JOIN orders o
+    ON a.id = o.account_id
+    GROUP BY a.name
+    HAVING SUM(o.standard_amt_usd + o.gloss_amt_usd + o.poster_amt_usd) > 30000
+        -- There are 204 accounts that came up
+
+
+    -- Which accounts spent less than 1,000 usd total across all orders?
+    SELECT a.name, SUM(o.standard_amt_usd + o.gloss_amt_usd + o.poster_amt_usd) AS orders_total_usd
+    FROM accounts a
+    JOIN orders o
+    ON a.id = o.account_id
+    GROUP BY a.name
+    HAVING SUM(o.standard_amt_usd + o.gloss_amt_usd + o.poster_amt_usd) < 1000
+        -- There are 3 accounts that came up.
+
+
+    -- Which account has spent the most with us?
+    SELECT a.name, SUM(o.standard_amt_usd + o.gloss_amt_usd + o.poster_amt_usd) AS orders_total_usd
+    FROM accounts a
+    JOIN orders o
+    ON a.id = o.account_id
+    GROUP BY a.name
+    HAVING SUM(o.standard_amt_usd + o.gloss_amt_usd + o.poster_amt_usd) > 30000
+    ORDER BY orders_total_usd DESC
+    LIMIT 1;
+        -- EOG Resources
+
+
+    -- Which account has spent the least with us?
+    SELECT a.name, SUM(o.standard_amt_usd + o.gloss_amt_usd + o.poster_amt_usd) AS orders_total_usd
+    FROM accounts a
+    JOIN orders o
+    ON a.id = o.account_id
+    GROUP BY 1
+    HAVING SUM(o.standard_amt_usd + o.gloss_amt_usd + o.poster_amt_usd) < 1000
+    ORDER BY 2
+    LIMIT 1;
+        -- Nike
+
+
+    -- Which accounts used facebook as a channel to contact customers more than 6 times?
+    SELECT a.name, COUNT(w.channel) AS web_channel_count
+    FROM accounts a
+    JOIN web_events w
+    ON a.id = w.account_id
+    GROUP BY a.name, w.channel
+    HAVING w.channel = 'facebook' AND COUNT(w.channel) > 6;
+        -- there 46 accounts which used facebook channel to contact customers more than 6 times.
+
+
+    -- Which account used facebook most as a channel?
+    SELECT a.name, COUNT(w.channel) AS web_channel_count
+    FROM accounts a
+    JOIN web_events w
+    ON a.id = w.account_id
+    GROUP BY a.name, w.channel
+    HAVING w.channel = 'facebook' AND COUNT(w.channel) > 6
+    ORDER BY 2 DESC
+    LIMIT 1;  
+        -- Gilead Sciences and it used it for 16 times 
+
+
+    -- Which channel was most frequently used by most accounts?
+    SELECT a.name, w.channel, COUNT(w.channel) AS web_channel_count
+    FROM accounts a
+    JOIN web_events w
+    ON a.id = w.account_id
+    GROUP BY a.name, w.channel
+    ORDER BY 2 DESC; 
+        -- direct channel, which was used for 5,298 times.
 
 
 
